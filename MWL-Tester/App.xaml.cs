beb17490677806aef991951a67c3c4d5a7411ee7
Sonny Pate <8129213+sonnypate@users.noreply.github.com>
2024-03-24 10:26:00 -0700

    Initial commit. Basic UI built

---
+++ MWL-Tester/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using FellowOakDicom;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Config;
+using NLog.Targets;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MWL_Tester
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Initialize log manager.
+            new DicomSetupBuilder()
+                .RegisterServices(services => services.AddLogging(logging => logging.AddProvider(new NLog.))
+               ).Build();
+
+                base.OnStartup(e);
+        }
+
+        
+    }
+
+}
---
+++ MWL-Tester/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using FellowOakDicom.Network.Client;
+using FellowOakDicom.Network;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MWL_Tester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Test_Click(object sender, RoutedEventArgs e)
+        {
+            var client = DicomClientFactory.Create(ServerIpAddress.Text, int.Parse(ServerPort.Text), false, CallingAET.Text, ServerAET.Text);
+            client.NegotiateAsyncOps();
+            for (int i = 0; i < 10; i++)
+                await client.AddRequestAsync(new DicomCEchoRequest());
+            await client.SendAsync();
+            
+        }
+
+    }
+}
