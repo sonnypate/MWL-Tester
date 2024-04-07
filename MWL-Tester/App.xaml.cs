e23dfaefb4a61bafac3ac40da605da9a9b2ecde7
Sonny Pate <8129213+sonnypate@users.noreply.github.com>
2024-04-06 20:30:15 -0700

    Added logging Set up basic DICOM testing.

--- MWL-Tester/App.xaml.cs
+++ MWL-Tester/App.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using FellowOakDicom;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NLog.Config;
-using NLog.Targets;
-using System.Configuration;
-using System.Data;
+using Serilog;
+using Serilog.Enrichers;
+using System.IO;
 using System.Windows;
+using ILogger = Serilog.ILogger;
 
 namespace MWL_Tester
 {
@@ -14,19 +14,43 @@ namespace MWL_Tester
     /// </summary>
     public partial class App : Application
     {
-        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
-
         protected override void OnStartup(StartupEventArgs e)
         {
-            // Initialize log manager.
-            new DicomSetupBuilder()
-                .RegisterServices(services => services.AddLogging(logging => logging.AddProvider(new NLog.))
-               ).Build();
+            var logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"MWL-Tester\Logs\Trace.log");
 
-                base.OnStartup(e);
+            var serilogLogger = ConfigureLogging();
+
+            new DicomSetupBuilder()
+                .RegisterServices(services => services.AddLogging(logging => logging.AddSerilog(serilogLogger)))
+                .Build();
+
+            Log.Information("Hello, world!");
+            base.OnStartup(e);
         }
 
-        
-    }
+        public ILogger ConfigureLogging()
+        {
+            var logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"MWL-Tester\Logs\Trace.log");
 
+            var loggerConfig = new LoggerConfiguration()
+                //Enrich each log message with the machine name
+                .Enrich.With<MachineNameEnricher>()
+                //Accept verbose output  (there is effectively no filter)
+                .MinimumLevel.Verbose()
+                //Also write out to a file based on the date and restrict these writes to warnings or worse (warning, error, fatal)
+                .WriteTo.File(logPath, 
+                    global::Serilog.Events.LogEventLevel.Verbose, 
+                    rollingInterval: RollingInterval.Day,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} | [{Level:u3}] | {Message:lj}{NewLine}{Exception}");
+
+            var logger = loggerConfig
+                //Take all of that configuration and make a logger
+                .CreateLogger();
+
+            //Stash the logger in the global Log instance for convenience
+            global::Serilog.Log.Logger = logger;
+
+            return logger;
+        }
+    }
 }
--- MWL-Tester/MainWindow.xaml.cs
+++ MWL-Tester/MainWindow.xaml.cs
@@ -1,15 +1,9 @@
-﻿using FellowOakDicom.Network.Client;
-using FellowOakDicom.Network;
-using System.Text;
+﻿using FellowOakDicom.Network;
+using FellowOakDicom.Network.Client;
+using Serilog;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MWL_Tester
 {
@@ -18,22 +12,73 @@ namespace MWL_Tester
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        ILogger _logger;
+        CancellationTokenSource _cts = new CancellationTokenSource();
 
         public MainWindow()
         {
+            _logger = Log.ForContext<MainWindow>();
             InitializeComponent();
         }
 
         private async void Test_Click(object sender, RoutedEventArgs e)
         {
-            var client = DicomClientFactory.Create(ServerIpAddress.Text, int.Parse(ServerPort.Text), false, CallingAET.Text, ServerAET.Text);
-            client.NegotiateAsyncOps();
-            for (int i = 0; i < 10; i++)
-                await client.AddRequestAsync(new DicomCEchoRequest());
-            await client.SendAsync();
-            
+            //TODO: Add cancellation
+            await TestDicomConnection();
         }
 
+        private async Task TestDicomConnection()
+        {
+            var client = DicomClientFactory.Create(ServerIpAddress.Text, int.Parse(ServerPort.Text), false, CallingAET.Text, ServerAET.Text);
+            client.AssociationAccepted += Client_AssociationAccepted;
+            client.AssociationRequestTimedOut += Client_AssociationRequestTimedOut;
+
+            try
+            {
+                client.NegotiateAsyncOps();
+                for (int i = 0; i < 10; i++)
+                    await client.AddRequestAsync(new DicomCEchoRequest());
+                await client.SendAsync(_cts.Token);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error: {exception}", ex);
+            }
+        }
+
+        private void Client_AssociationRequestTimedOut(object? sender, FellowOakDicom.Network.Client.EventArguments.AssociationRequestTimedOutEventArgs e)
+        {
+            if (StatusText.Dispatcher.CheckAccess())
+            {
+                StatusText.Content = $"Connection timed out";
+            }
+            else
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    StatusText.Content = $"Connection timed out";
+                });
+            }
+        }
+
+        private void Client_AssociationAccepted(object? sender, FellowOakDicom.Network.Client.EventArguments.AssociationAcceptedEventArgs e)
+        {            
+            if (StatusText.Dispatcher.CheckAccess())
+            {
+                StatusText.Content = "Association accepted";
+            }
+            else
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    StatusText.Content = "Association accepted";
+                });
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
